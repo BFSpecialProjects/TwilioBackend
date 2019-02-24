e4fe5162081b3c50654a41e0f71ffdbe8ba77122
BFSpecialProjects <43890684+BFSpecialProjects@users.noreply.github.com>
2019-02-23 19:25:54 -0500

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 echo "Welcome to the NestWatch Installer!\n\n"
 
-# get the latest version of the program (in this case, proof of conecpt ver)
-echo "\n\n\n\nGetting the latest version of NestWatch...\n\n\n"
-
-wget https://github.com/BFSpecialProjects/TwilioBackend/blob/master/twilioPOC.py
-
 ### install dependencies ###
 # pip, if it isn't already installed
 echo "\n\n\nInstalling pip, if it isn't already here...\n\n\n"
@@ -22,4 +17,4 @@ pip install twilio
 echo "\n\n\nGetting the latest version of the Launcher...\n\n\n"
 wget https://github.com/BFSpecialProjects/TwilioBackend/blob/master/launch.sh
 
-echo "\n\n\n\All done! Run ./launch.sh to get started."
+echo "\n\n\nAll done! Run ./launch.sh to get started."
