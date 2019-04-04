652b34d257330279939c0887657c6170fd3dd8b1
BFSpecialProjects <43890684+BFSpecialProjects@users.noreply.github.com>
2019-04-04 18:56:37 -0400

    Update NestWatch.py

--- NestWatch.py
+++ NestWatch.py
@@ -18,8 +18,11 @@ import psycopg2
 app = Flask(__name__)
 @app.route("/sms", methods = ['GET', 'POST'])
 
-# method called if teacher sends report
-def handleReport():
+# method to verify threat
+def verifyThreat(alert, sender):
+	# create verification message
+	alert = alert + " WHEN IT IS SAFE TO DO SO, please verify this threat by replying to this message."
+	
 	# connect to database and instantiate cursor
 	connection = psycopg2.connect(host="localhost",
                                   database="nestwatch",
@@ -32,17 +35,42 @@ def handleReport():
 	cursor.execute("SELECT phone_number FROM teachers")
 	teachers = str(cursor.fetchall())
 	
+	# iterate through teachers list and send verification message to all but sender
+	for row in teachers:
+		if not sender:
+			message = client.message.create(
+				to = teachers[row],
+				from_ = "+19193354640",
+				body = alert)
+			
+			print(message.sid)
+			
+# method called if teacher sends report
+def handleReport():
+	# connect to database and instantiate cursor
+	connection = psycopg2.connect(host="localhost",
+                                  database="nestwatch",
+                                  port="5432",
+                                  user="postgres",
+                                  password="Rn83:xD4")
+	cursor = connection.cursor()
+	
+	# format response
+	# store message sent by teacher and pass it to parser
+	report = request.values.get('Body', None)
+			
+	# parse message and store as reply
+	alert = nw_parser.ParseReport(report)
+	
+	# read from database, looking for teachers' numbers
+	cursor.execute("SELECT phone_number FROM teachers")
+	teachers = str(cursor.fetchall())
+	
 	# iterate through list	
 	# if sender is a verified teacher, give them a response
 	sender = request.values.get('From')
 	for row in teachers:
 		if sender in teachers:
-			# store message sent by teacher and pass it to parser
-			report = request.values.get('Body', None)
-			
-			# parse message and store as reply
-			alert = nw_parser.ParseReport(report)
-			
 			# respond to teacher that reported threat
 			# init TwiML response
 			resp = MessagingResponse()
@@ -50,7 +78,11 @@ def handleReport():
 			# ask Carrington what to say to teachers who are reporting incidents
 			resp.message('Your report will look like this once sent: ' + alert)
 			
+			# TODO: function doesn't recognize parameters
+			# verify threat
+			verifyThreat(alert, sender)
+			
 			return str(resp)
-
+			
 if __name__ == "__main__":
 	app.run(debug=True)
