d292576026e2e6fdd760ca0866a8835f84bb2ba6
BFSpecialProjects <43890684+BFSpecialProjects@users.noreply.github.com>
2019-05-01 16:05:46 -0700

    Update nw_parser.py

--- nw_parser.py
+++ nw_parser.py
@@ -1,13 +1,13 @@
 # parser
 # TODO: add trailers and mods
+# TODO: hardcode locations
 # TODO: conditional replies
-
 
 def ParseReport(report = ''):
 	# determine threat type
 	# for now, predetermined list of threats
-	# verify with Carrington: if we wanted to test during lockdown,
-	# "lockdown" keyword would be used
+	# verify with Carrington: if we wanted to test during drill,
+	# "drill" keyword would be used
 	threats = ['shooter', 'fire', 'lockdown', 'animal', 'dog', 'student with a gun', 'student with gun']
 
 	for item in threats:
@@ -21,12 +21,17 @@ def ParseReport(report = ''):
 		if i.isdigit():
 			# append to location string
 			location += i
-			
+
 	# form a coherent sentence for alert
 	# ask Carrington what this message should say
 	alert = "Attention faculty and students: There is a(n) " + threat + " around the " + location + " part of the campus."
 
 	return alert
 
-if __name__ == "__main__":
-	ParseReport()
+def ParseTableLength(lengthString = ''):
+	import string
+	all = string.maketrans('','')
+	nodigs = all.translate(all, string.digits)
+	tableLength = int(lengthString.translate(all, nodigs))
+
+	return tableLength
